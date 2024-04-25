2c33c4d390d74dcd8fcd8c709b3fb31aa7708f0f
winniffy <winniffy@gmail.com>
2024-04-25 15:33:18 +0100

    let him cook

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import viteLogo from './assets/wlogo.svg'
+
+function App() {
+
+  return (
+    <>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" />
+        </a>
+        <h1>test font</h1>
+        <box-icon name="rocket"></box-icon>
+    </>
+  )
+}
+
+export default App
