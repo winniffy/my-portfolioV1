6f30caf54bf236e61e5feb17f411dd6da3909265
winniffy <winniffy@gmail.com>
2024-05-29 17:34:58 +0100

    add framer animation to mobile menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,77 +1,81 @@
-import { useState } from 'react';
-import './Navbar.css'
+import { useState } from "react";
+import "./Navbar.css";
+import { AnimatePresence, motion} from "framer-motion";
 
 // nav items array
 const NavItems = [
-    { item: 'HOME'},
-    { item: 'PROJECTS'},
-    { item: 'ABOUT ME'},
-    { item: 'CONTACT'},
+    { item: "HOME" },
+    { item: "PROJECTS" },
+    { item: "ABOUT ME" },
+    { item: "CONTACT" },
 ];
 
 const Navbar = () => {
-
     const [openMenu, setOpenMenu] = useState(false);
 
-    function showMenu(){
+    function showMenu() {
         setOpenMenu(!openMenu);
     }
 
-  return (
-    <>
+    return (
+        <>
         <nav className="navbar">
-
             {/* desktop menu */}
             <ul className="nav_group">
-                {NavItems.map((nav, index) => (
-                    <li className='nav_item' key={index}>
-                        <a href="#" className="nav_link" >
-                            {nav.item}
-                        </a>
-                    </li>
-                ))
-                }
+            {NavItems.map((nav, index) => (
+                <li className="nav_item" key={index}>
+                <a href="#" className="nav_link">
+                    {nav.item}
+                </a>
+                </li>
+            ))}
             </ul>
 
             {/* mobile nav */}
             <ul className="nav_group-mobile">
-                <h2 className="hero_logo hero_logo-mobile">w.</h2>
-                <p className="menu_hamburger" onClick={showMenu}>MENU</p>
+            <h2 className="hero_logo hero_logo-mobile">w.</h2>
+            <p className="menu_hamburger" onClick={showMenu}>
+                MENU
+            </p>
             </ul>
-
-
         </nav>
-        
+
         {/* mobile menu */}
-        {openMenu &&
-        <ul className="mobile_menu-container">
-
-            {/* mode toggler button */}
-            <li>
+        <AnimatePresence>
+            {openMenu && (
+            <motion.ul
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{duration: .2}}
+                exit={{ opacity: 0, x: -100 }}
+                className="mobile_menu-container"
+            >
+                {/* mode toggler button */}
+                <li>
                 <button className="mode_btn-mobile">DARK MODE</button>
-            </li>
+                </li>
 
-            {/* menu items */}
-            {NavItems.map((nav, index) => (
-                <li className='nav_item' key={index}>
-                    <a href="#" className="nav_link" >
-                        {nav.item}
+                {/* menu items */}
+                {NavItems.map((nav, index) => (
+                <li className="nav_item" key={index}>
+                    <a href="#" className="nav_link">
+                    {nav.item}
                     </a>
                 </li>
-            ))
-            }
+                ))}
 
-            {/* close menu button */}
-            <li>
-                <button onClick={showMenu}  className="close_btn-container">
-                    <box-icon name='x' size="4.5rem" color="#928F8E50"></box-icon>
-                    <p className='close_text'>CLOSE</p>
+                {/* close menu button */}
+                <li>
+                <button onClick={showMenu} className="close_btn-container">
+                    <box-icon name="x" size="4.5rem" color="#928F8E50"></box-icon>
+                    <p className="close_text">CLOSE</p>
                 </button>
-            </li>
-        </ul>
-        }
-    </>
-  )
-}
+                </li>
+            </motion.ul>
+            )}
+        </AnimatePresence>
+        </>
+    );
+};
 
-export default Navbar
+export default Navbar;
